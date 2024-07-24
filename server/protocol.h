f571000da13a51bc8d5d9504696e79cd28b932d8
ssuni00 <126534280+ssuni00@users.noreply.github.com>
2024-07-24 23:15:22 +0900

    Create protocol.h

---
+++ server/protocol.h
@@ -0,0 +1,16 @@
+// protocol.h
+#ifndef PROTOCOL_H
+#define PROTOCOL_H
+
+typedef struct
+{
+    int seq_num;
+    char data[512];
+} Packet;
+
+typedef struct
+{
+    int ack_num;
+} Ack;
+
+#endif // PROTOCOL_H
